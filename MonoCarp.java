53c3b2d7828d2f30ae98feee0e1665da51bb9574
NITHISH K P <nithish28zz@gmail.com>
2026-09-12 11:13:24 +0530

    Add Monocarp class with main method for input handling

---
+++ MonoCarp.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Monocarp{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            int[] a=new int[n];
+            int zeros=0;
+            for(int i=0;i<n;i++){
+                a[i]=sc.nextInt();
+                if(a[i]==0)
+                    zeros++;
+            }
+            if(zeros<2)
+                System.out.println(-1);
+            else
+                System.out.println(a[0]+a[n-1]);
+        }
+        sc.close();
+    }
+}
